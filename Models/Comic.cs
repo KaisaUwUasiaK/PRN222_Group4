0f5603d32a3ed2eb063c56fa9fcc5b5fecebd43f
tside07 <manhtrung0612@gmail.com>
2026-02-15 11:21:17 +0700

    Add dashboard and manage moderator screen in admin workflow

---
+++ Models/Comic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Group4_ReadingComicWeb.Models
+{
+
+    public class Comic
+    {
+        [Key]
+        public int ComicId { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; } = null!;
+
+        [Column(TypeName = "ntext")]
+        public string? Description { get; set; }
+
+        public int AuthorId { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public User Author { get; set; } = null!;
+
+        [Required]
+        [StringLength(50)]
+        public string Status { get; set; } = "Pending";
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        // Viewcount
+        public int ViewCount { get; set; } = 0;
+
+        // Navigation
+        public ICollection<Chapter> Chapters { get; set; } = new List<Chapter>();
+    }
+}
---
+++ Models/Log.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Group4_ReadingComicWeb.Models
+{
+    public class Log
+    {
+        [Key]
+        public int LogId { get; set; }
+
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; } = null!;
+
+        [Required]
+        [StringLength(500)]
+        public string Action { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
