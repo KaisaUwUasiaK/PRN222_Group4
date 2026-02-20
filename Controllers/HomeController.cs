6bc560591650ea9e37719ecba958473963c55afe
lehung <hungnlhe181622@fpt.edu.vn>
2026-02-20 16:20:15 +0700

    Create home page and fix add chapter bug

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,30 +1,41 @@
+using Group4_ReadingComicWeb.Models;
+using Group4_ReadingComicWeb.Models.Enum;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace Group4_ReadingComicWeb.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _environment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(AppDbContext context, IWebHostEnvironment environment)
         {
-            _logger = logger;
+            _context = context;
+            _environment = environment;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
 
-        // public IActionResult Privacy()
-        // {
-        //     return View();
-        // }
+    public async Task<IActionResult> Index()
+    {
+        var trendingComic = await _context.Comics
+            .Include(c => c.ComicTags).ThenInclude(ct => ct.Tag)
+            .Where(c => c.Status == ComicStatus.OnWorking.ToString() || c.Status == ComicStatus.Completed.ToString())
+            .OrderByDescending(c => c.ViewCount)
+            .FirstOrDefaultAsync();
 
-        // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        // public IActionResult Error()
-        // {
-        //     return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        // }
+        var newComics = await _context.Comics
+            .Where(c => c.Status == ComicStatus.OnWorking.ToString() || c.Status == ComicStatus.Completed.ToString())
+            .OrderByDescending(c => c.CreatedAt)
+            .Take(10)
+            .ToListAsync();
+
+        ViewBag.NewComics = newComics;
+        return View(trendingComic);
     }
+
+
+}
 }
