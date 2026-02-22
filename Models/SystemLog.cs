d59c9e343d976edcaaeb85ea7258b28cfff5a3a8
cuongnqhe173086 <cuongnqhe173086@fpt.edu.vn>
2026-02-22 15:16:07 +0700

    Add Admin Log feature

---
+++ Models/SystemLog.cs
@@ -0,0 +1,10 @@
+﻿namespace Group4_ReadingComicWeb.Models
+{
+    public class SystemLog
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Action { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
